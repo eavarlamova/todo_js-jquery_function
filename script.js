667d6cd3079f0172fe317938dccd4f5f4855a398
Ekaterina Varlamova <e.varlamova@dunice.net>
2021-01-28 20:19:33 +0300

    check todo

---
+++ script.js
@@ -0,0 +1,64 @@
+$(() => {
+  const MAX_TODO = 5;
+  const ENTER = 'Enter';
+
+  const $todoCheckAllTodo = $('.todo__check-all-todo');
+  const $todoAddInput = $('.todo__add-input');
+  const $todoAddButton = $('.todo__add-button');
+  const $todoList = $('.todo__list');
+
+  let todos = [];
+  const currentPage = 1;
+  const currentTab = 'all-tab';
+
+  const normolizeText = (text) => (
+    text
+      .trim()
+      .replace(/\u0026/gu, '&amp;')
+      .replace(/\u003C/gu, '&lt;')
+      .replace(/\u003E/gu, '&gt;')
+      .replace(/\u0022/gu, '&quot;')
+      .replace(/\u0027/gu, '&#x27;')
+      .replace(/\u002F/gu, '&#x2F;')
+  );
+
+  const mapArray = (currentId, key, value, array = todos) => array
+    .map((item) => (item.id === currentId ? { ...item, [key]: value } : item));
+
+  const render = (array = todos) => {
+    const renderString = array.reduce((str, { text, status, id }) => (`
+    ${str}
+    <li id="${id}">
+      <input type="checkbox" class="todo__check-todo" ${status && 'checked'}/>
+      <span class="todo__text-todo"> ${text} </span>
+      <button class="todo__delete-todo"> x </button>
+    </li>
+    `), '');
+    $todoList.html(renderString);
+  };
+
+  const addTodo = () => {
+    const newText = normolizeText($todoAddInput.val());
+    if (newText) {
+      const newTodo = {
+        text: newText,
+        status: false,
+        id: Math.random(),
+      };
+      todos.push(newTodo);
+      $todoAddInput.val('');
+      render();
+    }
+  };
+
+  const getParentId = (currentThis) => Number(currentThis.parent().attr('id'));
+  const checkTodo = function () {
+    const currentId = getParentId($(this));
+    const currentStatus = $(this).prop('checked');
+    todos = mapArray(currentId, 'status', currentStatus);
+  };
+
+  $todoAddButton.on('click', addTodo);
+  $todoAddInput.on('keypress', (event) => { if (event.key === ENTER) addTodo(); });
+  $todoList.on('change', '.todo__check-todo', checkTodo);
+});
